6e791cabe3cc60ce5649c506e7e27564a3d90a9d
Gergely Brautigam <skarlso777@gmail.com>
2017-12-31 08:25:53 +0100

    Simplified

--- install_utils.sh
+++ install_utils.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y htop
-sudo apt-get install -y vim
-sudo apt-get install -y git
-sudo apt-get install -y wget
-sudo apt-get install -y mc
-sudo apt-get install -y nmap
-sudo apt-get install -y lynx
-sudo apt-get install -y build-essential cmake
-sudo apt-get install -y python-dev
-sudo apt-get install -y ctags
-sudo apt-get install -y language-pack-en
+sudo apt-get install -y htop vim git wget mc nmap lynx build-essential cmake python-dev ctags language-pack-en
